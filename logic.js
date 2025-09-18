8d01a356df317978b1b86de82c575e2cb587c30f
xeipiDev <dev.juanpadin@gmail.com>
2025-09-18 08:24:42 -0300

    agregue accion de dibujo (hay que mejorar)

--- logic.js
+++ logic.js
@@ -1,11 +1,27 @@
-// crear los divs para los dibujos.
-const contenedorDibujos = document.querySelector('.contenedor-dibujos'); // obtener contenedor.
+// obtener referencia al contenedor de dibujos
+const contenedorDibujos = document.querySelector('.contenedor-dibujos');
 
-for (let i = 0; i < 20000; i++) {
-    const divDibujo = document.createElement('div');
-    divDibujo.classList.add('dibujo');
-    contenedorDibujos.appendChild(divDibujo);
-    divDibujo.addEventListener('click', () => {
-    
-    }); // agregar evento click a cada div.   
+let color = 'black'; // color por defecto
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+const crearCuadricula = (cantidad) => {
+    for (let i = 0; i < cantidad; i++) {
+        const divDibujo = document.createElement('div');
+        divDibujo.classList.add('dibujo');
+        contenedorDibujos.appendChild(divDibujo);
+        divDibujo.addEventListener('mouseover', accion);
+        divDibujo.addEventListener('mousedown', accion); // agregar evento click a cada div.
+    }
 }
+
+    const accion = (e) => {
+        if (e.type === 'mouseover' && !mouseDown)
+        if (e.target.classList.contains('dibujo')) {
+            e.target.style.backgroundColor = color;
+        }   
+    }
+
+crearCuadricula(20000); // crear 20000 divs.
