a782d8e0c5031033dd17411d0744f4030f4ed44e
xeipiDev <dev.juanpadin@gmail.com>
2025-09-18 01:40:33 -0300

    se agrega botones

--- logic.js
+++ logic.js
@@ -1,8 +1,11 @@
 // crear los divs para los dibujos.
 const contenedorDibujos = document.querySelector('.contenedor-dibujos'); // obtener contenedor.
 
-for (let i = 0; i < 50000; i++) {
+for (let i = 0; i < 20000; i++) {
     const divDibujo = document.createElement('div');
     divDibujo.classList.add('dibujo');
     contenedorDibujos.appendChild(divDibujo);
+    divDibujo.addEventListener('click', () => {
+    
+    }); // agregar evento click a cada div.   
 }
